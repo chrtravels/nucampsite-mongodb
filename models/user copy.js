1a7a43c9e0cefe564e7fc0925f98b1368d3a1c3a
Chris Reynoso <chrtravels@gmail.com>
2021-01-15 12:41:16 -0800

    Mongoose Population

--- models/user copy.js
+++ models/user copy.js
@@ -3,18 +3,10 @@ const passportlocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  firstname: {
-      type: String,
-      default: ''
-  },
-  lastname: {
-      type: String,
-      default: ''
-  },
-  admin: {
-      type: Boolean,
-      default: false
-  }
+    admin: {
+        type: Boolean,
+        default: false
+    }
 });
 
 userSchema.plugin(passportlocalMongoose);
