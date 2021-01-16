2f7ea83cc8e0d6902e4a8535c89b2b487c6bd254
Chris Reynoso <chrtravels@gmail.com>
2021-01-16 12:34:47 -0800

    Workshop 3

--- authenticate.js
+++ authenticate.js
@@ -38,3 +38,15 @@ exports.jwtPassport = passport.use(
 );
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+// Week 3 Workshop - verify if user has admin privileges
+
+exports.verifyAdmin = (req, res, next) => {
+    if (req.user.admin){
+        return next();
+    } else {
+        const err = new Error('You are not authorized to perform this operation!');
+        err.status = 403 ;
+        return next(err);
+    }
+};
