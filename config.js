ee8d3fabe92c3639e6c62729747f9e05cd7158f2
Chris Reynoso <chrtravels@gmail.com>
2021-01-19 12:53:52 -0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey' : '12345-67890-09876-54321',
-  'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+  'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+  'facebook' : {
+    clientId: '766533777312131',
+    clientSecret : '7a2e4d4b509c1b9a1f1bd150b881c296'
+  }
 }
